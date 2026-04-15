52731b7e7515ff0c190dc2ef3e819a117a18110e
ALPHA-TF141 <lmariaimmanuel@gmail.com>
2026-04-15 22:38:24 +0530

    Initialize React app with Create React App
    Add a new Create React App scaffold under ReactJS/my-app. Includes project metadata (package.json and lockfile), .gitignore, README, public assets, and source files (App, index, tests, web vitals) plus example components (Car, Garage). Provides standard npm scripts (start/test/build) and base configuration to begin development.

---
+++ ReactJS/my-app/src/components/Car.js
@@ -0,0 +1,8 @@
+function car() {
+    return (
+        <div>
+            <h2>Hi, I am a Car!</h2>
+        </div>
+    );
+}
+export default car;
---
+++ ReactJS/my-app/src/components/Garage.js
@@ -0,0 +1,10 @@
+import car from './Car'
+function Garage() {
+    return (
+        <div>
+            <h1>Who lives in my garage?</h1>
+            <car />
+        </div>
+    );
+}
+export default Garage;
